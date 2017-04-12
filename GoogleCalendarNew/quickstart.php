9093326fee81aab63bea18302bd31b991839a9de
Unknown <amber__12@hotmail.com>
2017-04-12 15:09:52 +0200

    Show all events from selected calendars

--- GoogleCalendarNew/quickstart.php
+++ GoogleCalendarNew/quickstart.php
@@ -75,29 +75,29 @@ function expandHomeDirectory($path) {
 }
 
 // Get the API client and construct the service object.
-$client = getClient();
-$service = new Google_Service_Calendar($client);
+// $client = getClient();
+// $service = new Google_Service_Calendar($client);
 
 
 // // Print the next 10 events on the user's calendar.
-$calendarId = 'primary';
-$optParams = array(
-  'maxResults' => 10,
-  'orderBy' => 'startTime',
-  'singleEvents' => TRUE,
-  'timeMin' => date('c'),
-);
-$results = $service->events->listEvents($calendarId, $optParams);
+// $calendarId = 'primary';
+// $optParams = array(
+//   'maxResults' => 10,
+//   'orderBy' => 'startTime',
+//   'singleEvents' => TRUE,
+//   'timeMin' => date('c'),
+// );
+// $results = $service->events->listEvents($calendarId, $optParams);
 
-if (count($results->getItems()) == 0) {
-  print "No upcoming events found.\n";
-} else {
-  print "Upcoming events:\n";
-  foreach ($results->getItems() as $event) {
-    $start = $event->start->dateTime;
-    if (empty($start)) {
-      $start = $event->start->date;
-    }
-    printf("%s (%s)\n", $event->getSummary(), $start);
-  }
-}
+// if (count($results->getItems()) == 0) {
+//   print "No upcoming events found.\n";
+// } else {
+//   print "Upcoming events:\n";
+//   foreach ($results->getItems() as $event) {
+//     $start = $event->start->dateTime;
+//     if (empty($start)) {
+//       $start = $event->start->date;
+//     }
+//     printf("%s (%s)\n", $event->getSummary(), $start);
+//   }
+// }
