714cc0b0e3b9f6483a0d60b4b356028fd8958015
Unknown <amber__12@hotmail.com>
2017-04-12 16:25:19 +0200

    Insert multiple events at once

--- GoogleCalendarNew/addEvents.php
+++ GoogleCalendarNew/addEvents.php
@@ -21,10 +21,19 @@
             'dateTime' => '2017-04-15T17:00:00-07:00',
             'timeZone' => 'Europe/Amsterdam',
         ),
+        'summary' => 'TestMultiple',
+        'start' => array(
+            'dateTime' => '2017-04-14T03:00:00-02:00',
+            'timeZone' => 'Europe/Amsterdam',
+        ),
+        'end' => array(
+            'dateTime' => '2017-04-14T05:00:00-02:00',
+            'timeZome' => 'Europe/Amsterdam',
+        ),
     ));
 
-$calendarId = 'csj8vhvrbdsl01m8vnjihpfve4@group.calendar.google.com';
-$event = $service->events->insert($calendarId, $event);
-printf('Event created: %s\n', $event->htmlLink);
+    $calendarId = 'csj8vhvrbdsl01m8vnjihpfve4@group.calendar.google.com';
+    $event = $service->events->insert($calendarId, $event);
+    printf('Event created: %s\n', $event->htmlLink);
 
 ?>
