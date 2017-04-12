681f073d5c1c5443c073623b6b0d1ad5a613506f
Unknown <amber__12@hotmail.com>
2017-04-12 15:23:13 +0200

    Insert event into calendar

---
+++ GoogleCalendarNew/addEvents.php
@@ -0,0 +1,30 @@
+<?php
+
+    require 'quickstart.php';
+
+    $client = getClient();
+    $service = new Google_Service_Calendar($client);
+
+    $name = 'testTask';
+    $deadline;
+    $timeNeeding;
+    $dayRange = 1;
+    $priority = 'high';
+
+    $event = new Google_Service_Calendar_Event(array(
+        'summary' => $name,
+        'start' => array(
+            'dateTime' => '2017-04-15T09:00:00-07:00',
+            'timeZone' => 'Europe/Amsterdam',
+        ),
+        'end' => array(
+            'dateTime' => '2017-04-15T17:00:00-07:00',
+            'timeZone' => 'Europe/Amsterdam',
+        ),
+    ));
+
+$calendarId = 'csj8vhvrbdsl01m8vnjihpfve4@group.calendar.google.com';
+$event = $service->events->insert($calendarId, $event);
+printf('Event created: %s\n', $event->htmlLink);
+
+?>
