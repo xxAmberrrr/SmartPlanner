7edb27e049f07ffd9a6fcbeb9095d476550a2160
Unknown <amber__12@hotmail.com>
2017-04-13 10:27:35 +0200

    Insert amount of events based on days needing for task

--- GoogleCalendarNew/addEvents.php
+++ GoogleCalendarNew/addEvents.php
@@ -8,10 +8,13 @@
     $name = 'testTask';
     $deadline;
     $timeNeeding;
-    $dayRange = 1;
+    $daysNeeding = 3;
     $priority = 'high';
 
-    $event = new Google_Service_Calendar_Event(array(
+    $calendarId = 'csj8vhvrbdsl01m8vnjihpfve4@group.calendar.google.com';
+
+    for($i = 0; $i < $daysNeeding; $i++) {
+        $event = new Google_Service_Calendar_Event(array(
         'summary' => $name,
         'start' => array(
             'dateTime' => '2017-04-15T09:00:00-07:00',
@@ -21,19 +24,10 @@
             'dateTime' => '2017-04-15T17:00:00-07:00',
             'timeZone' => 'Europe/Amsterdam',
         ),
-        'summary' => 'TestMultiple',
-        'start' => array(
-            'dateTime' => '2017-04-14T03:00:00-02:00',
-            'timeZone' => 'Europe/Amsterdam',
-        ),
-        'end' => array(
-            'dateTime' => '2017-04-14T05:00:00-02:00',
-            'timeZome' => 'Europe/Amsterdam',
-        ),
-    ));
+        ));
 
-    $calendarId = 'csj8vhvrbdsl01m8vnjihpfve4@group.calendar.google.com';
-    $event = $service->events->insert($calendarId, $event);
-    printf('Event created: %s\n', $event->htmlLink);
+        $event = $service->events->insert($calendarId, $event);
+        printf('Event(s) created: %s\n', $event->htmlLink);
+    }
 
 ?>
