b22deb289aae7f5e71bf21294f62c4542351978e
Unknown <amber__12@hotmail.com>
2017-04-18 14:15:39 +0200

    getEvents in function

--- getEvents.php
+++ getEvents.php
@@ -4,37 +4,38 @@
     $timeRange;
 
     // Get the API client and construct the service object.
-    $client = getClient();
-    $service = new Google_Service_Calendar($client);
+    function getEvents() {
+        $client = getClient();
+        $service = new Google_Service_Calendar($client);
 
 
-    //List of calendars
-    $calendarList = $service->calendarList->listCalendarList();
+        //List of calendars
+        $calendarList = $service->calendarList->listCalendarList();
 
-    $calendars = array(); //Empty array for calendars
-    $calendarsSelected = array(); //Empty array for selected calendars
+        $calendars = array(); //Empty array for calendars
+        $calendarsSelected = array(); //Empty array for selected calendars
 
-    array_push($calendarsSelected, '228iua7de1js8if439cgsgk4dc@group.calendar.google.com'); //MT3C
-    //array_push($calendarsSelected, 'xxamberrrr12@gmail.com'); //Amber Hoogland
-    array_push($calendarsSelected, 'ihl73aqpljlu9u67srth0657s8@group.calendar.google.com'); //School
-    array_push($calendarsSelected, 'csj8vhvrbdsl01m8vnjihpfve4@group.calendar.google.com'); //SmartPlanner
-    array_push($calendarsSelected, 'k6m04v2tp7ortm5ol5kg8clkuk@group.calendar.google.com'); //Werk
+        array_push($calendarsSelected, '228iua7de1js8if439cgsgk4dc@group.calendar.google.com'); //MT3C
+        //array_push($calendarsSelected, 'xxamberrrr12@gmail.com'); //Amber Hoogland
+        array_push($calendarsSelected, 'ihl73aqpljlu9u67srth0657s8@group.calendar.google.com'); //School
+        array_push($calendarsSelected, 'csj8vhvrbdsl01m8vnjihpfve4@group.calendar.google.com'); //SmartPlanner
+        array_push($calendarsSelected, 'k6m04v2tp7ortm5ol5kg8clkuk@group.calendar.google.com'); //Werk
 
-    //Params to filter
-    $optParams = array(
-    'orderBy' => 'starttime',
-    'singleEvents' => TRUE,
-    'timeMin' => date('c'),
-    'showDeleted' => FALSE,
-    ); 
+        //Params to filter
+        $optParams = array(
+        'orderBy' => 'starttime',
+        'singleEvents' => TRUE,
+        'timeMin' => date('c'),
+        'showDeleted' => FALSE,
+        ); 
 
-    while(true) {
-    //use calendarsSelected when interface is ready
-    // foreach ($calendarList->getItems() as $calendarListEntry) {
-    //   array_push($calendarsSelected, $calendarListEntry->getId());
-    // }
+        while(true) {
+        //use calendarsSelected when interface is ready
+        // foreach ($calendarList->getItems() as $calendarListEntry) {
+        //   array_push($calendarsSelected, $calendarListEntry->getId());
+        // }
 
-    //For every calendar in calendarsSelected show the events
+        //For every calendar in calendarsSelected show the events
         for($i = 0; $i < count($calendarsSelected); $i++) {
             $results = $service->events->listEvents($calendarsSelected[$i], $optParams);
 
@@ -62,7 +63,7 @@
                 }
             }
 
-            //printf("%s (%s)(%s)\n", $event->getSummary(), $start, $end);
+            printf("%s (%s)(%s)\n", $event->getSummary(), $start, $end);
             }
         }
 
@@ -76,5 +77,8 @@
         else {
             break;
         }
+        }
     }
+
+    getEvents();
 ?>
