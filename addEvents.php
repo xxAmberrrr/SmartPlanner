5d8c0030f4bf06c9d043ad26e61de8d57ab95700
Unknown <amber__12@hotmail.com>
2017-04-18 12:11:49 +0200

    Events randomly insert between now and deadline between 9 to 5

--- addEvents.php
+++ addEvents.php
@@ -7,28 +7,42 @@
 
     $name = 'testTask';
     $planningTimeAvailable;
-    $deadline;
+    $deadline = '2017-04-28';
     $timeNeeding;
     $daysNeeding = 3;
-    $priority = 'high';
+    $dateNow = date('Y-m-d');
 
-    //To do: 
-    //check date function php to set date deadline,
-    //check random php,
-    //plan between $planningTimeAvailable (9 to 5),
-    //plan random in the weeks till $deadline
+    $minTime = '09:00:00';
+    $maxTime = '17:00:00';
 
+    function rand_date($min_date, $max_date, $min_time, $max_time) {
+        $min_epoch = strtotime($min_date);
+        $max_epoch = strtotime($max_date);
+        $min_time_epoch = strtotime($min_time);
+        $max_time_epoch = strtotime($max_time);
+
+        $rand_epoch = rand($min_epoch, $max_epoch);
+        $rand_time_epoch = rand($min_time_epoch, $max_time_epoch);
+
+        $date = date('Y-m-d', $rand_epoch);
+        $time = date('H:i:s', $rand_time_epoch);
+
+        return $date . 'T' . $time;
+    }
+    
     $calendarId = 'csj8vhvrbdsl01m8vnjihpfve4@group.calendar.google.com';
 
     for($i = 0; $i < $daysNeeding; $i++) {
+        $dateStart = rand_date($dateNow, $deadline, $minTime, $maxTime);
+
         $event = new Google_Service_Calendar_Event(array(
         'summary' => $name,
         'start' => array(
-            'dateTime' => '2017-04-15T09:00:00-07:00',
+            'dateTime' => $dateStart,
             'timeZone' => 'Europe/Amsterdam',
         ),
         'end' => array(
-            'dateTime' => '2017-04-15T17:00:00-07:00',
+            'dateTime' => date('Y-m-d\TH:i:s',strtotime('+2 hours',strtotime($dateStart))),
             'timeZone' => 'Europe/Amsterdam',
         ),
         ));
