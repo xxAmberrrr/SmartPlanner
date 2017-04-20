3133602733d2e07ae998f82fdd7b84e57be9b8af
Unknown <amber__12@hotmail.com>
2017-04-20 16:38:39 +0200

    Get all busy time

--- getFreeBusy.php
+++ getFreeBusy.php
@@ -2,62 +2,58 @@
 
     require 'eventFunctions.php';
 
-     $client = getClient();
-     $service = new Google_Service_Calendar($client);
-
-    //getEvents();
-
-    //$eventName, $days, $deadline, $minTime, $maxTime
-    //addEvent('Amber test 2', 3, '2017-04-28', '09:00:00', '17:00:00');
-
-//    $params = new stdClass();
-//    $params->property = "hellow";
-//    json_encode($params);
-//    die(0);
-//
-//    $newParams = json_encode($params);
-//
-//    //print($newParams);
-//
-//    $request = new Google_Service_Calendar_FreeBusyRequest();
-//
-//    $result = $service->freebusy->query($request, $params);
-//
-//
-//    $freebusy = new Google_Service_Calendar_FreeBusyRequest();
-//
-//    $query = $service->freebusy->query($freebusy, $newParams);
-//
-//    json_encode($query);
-
+    $client = getClient();
+    $service = new Google_Service_Calendar($client);
 
     $freebusy = new Google_Service_Calendar_FreeBusyRequest();
     $freebusy->setTimeMin("2017-04-18T13:00:00+01:00");
     $freebusy->setTimeMax("2017-04-28T13:00:00+01:00");
     $freebusy->setTimeZone('Europe/Amsterdam');
 
-    $item = new Google_Service_Calendar_FreeBusyRequestItem();
-    $item->setId('k6m04v2tp7ortm5ol5kg8clkuk@group.calendar.google.com');
+    $workCal = new Google_Service_Calendar_FreeBusyRequestItem();
+    $workCal->setId('k6m04v2tp7ortm5ol5kg8clkuk@group.calendar.google.com');
 
-    $freebusy->setItems(array($item));
+    $smartPlannerCal = new Google_Service_Calendar_FreeBusyRequestItem();
+    $smartPlannerCal->setId('csj8vhvrbdsl01m8vnjihpfve4@group.calendar.google.com');
+
+    $schoolCal = new Google_Service_Calendar_FreeBusyRequestItem();
+    $schoolCal->setId('ihl73aqpljlu9u67srth0657s8@group.calendar.google.com');
+
+    $scheduleCal = new Google_Service_Calendar_FreeBusyRequestItem();
+    $scheduleCal->setId('228iua7de1js8if439cgsgk4dc@group.calendar.google.com');
+
+    $freebusy->setItems(array($workCal, $smartPlannerCal, $schoolCal, $scheduleCal));
 
     $query = $service->freebusy->query($freebusy);
 
-    print_r(json_encode($query));
-    //var_dump($query);
+    $queryWork = $query->getCalendars()["k6m04v2tp7ortm5ol5kg8clkuk@group.calendar.google.com"]["modelData"]["busy"];
+    $querySmartPlanner = $query->getCalendars()["csj8vhvrbdsl01m8vnjihpfve4@group.calendar.google.com"]["modelData"]["busy"];
+    $querySchool = $query->getCalendars()["ihl73aqpljlu9u67srth0657s8@group.calendar.google.com"]["modelData"]["busy"];
+    $querySchedule = $query->getCalendars()["228iua7de1js8if439cgsgk4dc@group.calendar.google.com"]["modelData"]["busy"];
 
+    for($i = 0; $i < count($queryWork); $i++) {
+        var_dump($queryWork[$i]["start"]);
+        var_dump($queryWork[$i]["end"]);
+    }
 
-//    $date_from = '2017-04-18T13:00:00+01:00';
-//    $date_to = '2017-04-28T13:00:00+01:00';
-//
-//    $freebusy_req = new Google_Service_Calendar_FreeBusyRequest();
-//    $freebusy_req->setTimeMin(date(DateTime::ATOM, strtotime($date_from)));
-//    $freebusy_req->setTimeMax(date(DateTime::ATOM, strtotime($date_to)));
-//    $freebusy_req->setTimeZone('Europe/Amsterdam');
-//    $item = new Google_Service_Calendar_FreeBusyRequestItem();
-//    $item->setId('ihl73aqpljlu9u67srth0657s8@group.calendar.google.com');
-//    $freebusy_req->setItems(array($item));
-//    $query = $service->freebusy->query($freebusy_req);
-//
-//    echo json_encode($query);
+    for($i = 0; $i < count($querySmartPlanner); $i++) {
+        var_dump($querySmartPlanner[$i]["start"]);
+        var_dump($querySmartPlanner[$i]["end"]);
+    }
+
+    for($i = 0; $i < count($querySchool); $i++) {
+        var_dump($querySchool[$i]["start"]);
+        var_dump($querySchool[$i]["end"]);
+    }
+
+    for($i = 0; $i < count($querySchedule); $i++) {
+        var_dump($querySchedule[$i]["start"]);
+        var_dump($querySchedule[$i]["end"]);
+    }
+
+    //var_dump($queryWork);
+    //var_dump($querySmartPlanner);
+    //var_dump($querySchool);
+    //var_dump($scheduleCal);
+
 ?>
