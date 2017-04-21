e09d9539f2be2e8e7403b0bd768a901baddd350c
Unknown <amber__12@hotmail.com>
2017-04-21 12:45:16 +0200

    Little nicer interface

--- index.php
+++ index.php
@@ -6,19 +6,21 @@
 
         <title>Smart Planner</title>
 
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
     </head>
 
     <body>
 
         <form action="getEvents.php">
-            <input type="submit" value="Get your events"/>
+            <input type="submit" class="btn btn-primary" value="Get your events"/>
         </form>
 
         <br>
         <br>
 
         <form action="getFreeBusy.php">
-            <input type="submit" value="Get your busy time data"/>
+            <input type="submit" class="btn btn-primary" value="Get your busy time data"/>
         </form>
 
         <br>
@@ -38,7 +40,7 @@
             <input type="date" name="deadline"/>
             <br>
             <br>
-            <input type="submit" value="Insert a event"/>
+            <input type="submit" value="Insert a event" class="btn btn-primary"/>
         </form>
 
     </body>
